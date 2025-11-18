a50c39d4c0dd6bc06b2bb0bc81b7c0bdcbb75afc
Harald Schneider <h_schneider@marketmix.com>
2025-11-18 14:46:53 +0100

    Migrated to .NET10

--- _dotnet/publish.sh
+++ _dotnet/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd neutralino-blazor
 dotnet publish -c Release
-cp -R bin/Release/net9.0/publish/wwwroot/ ../../resources
+cp -R bin/Release/net10.0/publish/wwwroot/ ../../resources
 cp ../resources/index.html ../../resources
